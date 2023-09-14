1bbc5f12e11e900fa3d5517c36e3d03b66f4746e
Ariyawat-N <133693268+Ariyawat-N@users.noreply.github.com>
2023-09-14 13:25:00 +0700

    final

--- src/HomeUser.jsx
+++ src/HomeUser.jsx
@@ -9,9 +9,35 @@ const HomeUser = () => {
   return (
     <div>
       <App />
-  
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Position</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>Manager</td>
+          </tr>
+            <tr>
+              <td>Jane</td>
+              <td>Doe</td>
+              <td>CEO</td>
+            </tr>
+          <tr>
+              <td>Jack</td>
+              <td>Doe</td>
+              <td>CTO</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
 export default HomeUser
